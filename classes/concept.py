6332b43726d572f24f1da602ecad159ef84b9604
DarthVader7 <ambrishrawat@gmail.com>
2016-11-26 18:39:37 +0000

    general updates

--- classes/concept.py
+++ classes/concept.py
@@ -1,3 +1,7 @@
+def make_Bad_Char_SR_table(value):
+    pass
+
+
 class Concept(str):
     '''
     class for Concept
@@ -6,12 +10,16 @@ class Concept(str):
     other meta data can be included like
 
     - source language
-    - other structure that relates it to concepts like via a knowledge graph
+    - other structure that relates it to concepts like via a knowledge graph/ontology
+    - this also allows for adding preprcessing steps eg. shift rules in BMSearch
 
     '''
 
     def __new__(cls, value, *args, **kwargs):
         return super(Concept, cls).__new__(cls, value)
 
-    
+
+    def __init__(self, value):
+        self.badCharacterShiftRule = make_Bad_Char_SR_table(value)
+        self.goodSuffixRule = make_Bad_Char_SR_table(value)
 
--- classes/sentence.py
+++ classes/sentence.py
@@ -11,6 +11,5 @@ class Sentence(str):
     '''
 
     def __new__(cls, value, *args, **kwargs):
-        print('Yoda',value)
         return super(Sentence, cls).__new__(cls, value)
 
